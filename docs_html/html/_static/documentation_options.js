681f3a3a6b22fcd33c00d115846b5d77eb338be4
Michael Slugocki <mslugocki@Michaels-MacBook-Pro.local>
2024-04-22 15:57:48 -0400

    updated doc version

--- docs_html/html/_static/documentation_options.js
+++ docs_html/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.3',
+    VERSION: '2.4',
     LANGUAGE: 'English',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
